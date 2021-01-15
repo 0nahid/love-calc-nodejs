3d64d5774e5aef17aa891558a1ea3448fb3e35f5
Nahid Hassan <41489906+0nahid@users.noreply.github.com>
2021-01-15 21:16:43 +0600

    updated ui

--- lovecalc.js
+++ lovecalc.js
@@ -1,15 +1,12 @@
-prompt("What is your name?");
-prompt("What is their name?");
-
 var loveScore= Math.random()*100;
 loveScore=Math.floor(loveScore)+1;
 console.log(loveScore);
 
 if (loveScore > 70){
-alert("Your love scoor is "+ loveScore+ "% ." + " You love each other like Romeo & Juliet");
+    document.querySelector(".demo1").innerHTML= "<h1>Your love scoor is "+ loveScore+ "% ." + "<br> You love each other like Romeo & Juliet</h1>";
 }
 if (loveScore > 30 && loveScore<=70){
-     alert("Your love score is "+ loveScore+ "%");
+    document.querySelector(".demo1").innerHTML= "<h1>Your love score is "+ loveScore+ "% ."+"<br> Your love is not pure.  You'ld give more time to your partner .</h1>";
 } if (loveScore <=30){
-    alert("Your love score is "+ loveScore+ "%" +" You'll together like oil and water");
+    document.querySelector(".demo1").innerHTML= "<h1>Your love score is "+ loveScore+ "% ." +"<br> You'll together like oil and water</h1>";
 }
