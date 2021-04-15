5ccb80d3127a40f7fd802eeec33ea6b8372fde69
Nahid Hassan <41489906+0nahid@users.noreply.github.com>
2021-04-15 18:02:00 +0600

    validation added but it didnt work

--- app.js
+++ app.js
@@ -56,5 +56,5 @@ app.post("/", function(req,res){
 });
 
 app.listen(process.env.PORT || 3000, function(){
-    console.log('Server is running on Heroku ');
+    console.log('Server is running on local & heroku both ');
 })
--- public/js/lovecalc.js
+++ public/js/lovecalc.js
@@ -1,12 +1,13 @@
-var loveScore= Math.random()*100;
-loveScore=Math.floor(loveScore)+1;
+var loveScore = Math.random() * 100;
+loveScore = Math.floor(loveScore) + 1;
 console.log(loveScore);
 
-if (loveScore > 70){
-    document.querySelector(".demo1").innerHTML= "<h1>Your love scoor is "+ loveScore+ "% ." + "<br> You love each other like Romeo & Juliet</h1>";
+if (loveScore > 70) {
+    document.querySelector(".demo1").innerHTML = "<h1>Your love score is " + loveScore + "% ." + "<br> You love each other like Romeo & Juliet</h1>";
 }
-if (loveScore > 30 && loveScore<=70){
-    document.querySelector(".demo1").innerHTML= "<h1>Your love score is "+ loveScore+ "% ."+"<br> Your love is not pure.  You'ld give more time to your partner .</h1>";
-} if (loveScore <=30){
-    document.querySelector(".demo1").innerHTML= "<h1>Your love score is "+ loveScore+ "% ." +"<br> You'll together like oil and water</h1>";
+if (loveScore > 30 && loveScore <= 70) {
+    document.querySelector(".demo1").innerHTML = "<h1>Your love score is " + loveScore + "% ." + "<br> Your love is not pure.  You'ld give more time to your partner .</h1>";
+}
+if (loveScore <= 30) {
+    document.querySelector(".demo1").innerHTML = "<h1>Your love score is " + loveScore + "% ." + "<br> You'll together like oil and water</h1>";
 }
